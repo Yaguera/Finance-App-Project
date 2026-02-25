73f0a286f7269cb90b43847f96d28c0c3c6ecfd3
Yaguera <ya-gotre@hotmail.com>
2026-02-25 20:53:57 -0300

    chore: add pre-commit git hook setup

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+const Configuration = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    // Você pode adicionar regras personalizadas aqui se quiser
+  },
+};
+
+export default Configuration;
---
+++ index.js
@@ -0,0 +1,3 @@
+const name = "teste";
+
+console.log(name);
