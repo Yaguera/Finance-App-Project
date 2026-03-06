7eb53ed09ac5d6a4ccc7840fed32bb20f457c485
Yaguera <ya-gotre@hotmail.com>
2026-03-06 20:36:08 -0300

    feat: add CreateUseCase

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,27 @@
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
+import { PostgresCreateUserRepository } from "../repositories/postgres/create-user";
+
+export class CreateUserUseCase {
+  async execute(createUserParams) {
+    // TODO: verify if the user email is already in use
+
+    // generate user ID
+    const userId = uuidv4();
+
+    // encrypt password
+    const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
+
+    // insert the user in the database
+    const user = {
+      ...createUserParams,
+      id: userId,
+      password: hashedPassword,
+    };
+
+    // call repository
+    const postgresCreateUserRepository = new PostgresCreateUserRepository();
+    const createdUser = await postgresCreateUserRepository.execute(user);
+    return createdUser;
+  }
+}
