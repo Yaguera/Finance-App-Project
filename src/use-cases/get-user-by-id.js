cd333e7fe3a19e9cbbf811e528f9b41f16749385
Yaguera <ya-gotre@hotmail.com>
2026-03-16 16:38:39 -0300

    feat: add PostGetUserByIdUseCase

---
+++ src/use-cases/get-user-by-id.js
@@ -0,0 +1,11 @@
+import { PostgresCreateUserRepository } from "../repositories/postgres/create-user";
+
+export class GetUserByIdUseCase {
+  async execute(userId) {
+    const getUserByIdRepository = new PostgresCreateUserRepository();
+
+    const user = await getUserByIdRepository.execute(userId);
+
+    return user;
+  }
+}
