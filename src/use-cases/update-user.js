deafec7404da38301a2e3f56a2b2d0ea07d74a37
Yaguera <ya-gotre@hotmail.com>
2026-03-16 19:40:56 -0300

    feat: add UpdateUserUseCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,34 @@
+import bcrypt from "bcrypt";
+import { EmailAlreadyInUseError } from "../erros/user.js";
+import { PostgresGetUserByEmailRepository } from "../repositories/postgres/get-user-by-email.js";
+import { PostgresUpdateUserRepository } from "../repositories/postgres/update-user.js";
+
+export class UpdateUserUseCase {
+  async execute(userId, updateUserParams) {
+    if (updateUserParams.email) {
+      const postgresGetUserByEmailRepository =
+        new PostgresGetUserByEmailRepository();
+      const userWithProviderEmail =
+        await postgresGetUserByEmailRepository.execute(updateUserParams.email);
+      if (userWithProviderEmail) {
+        throw new EmailAlreadyInUseError(updateUserParams.email);
+      }
+    }
+    const user = {
+      ...updateUserParams,
+    };
+
+    if (updateUserParams.password) {
+      const hashedPassword = await bcrypt.hash(updateUserParams.password, 10);
+      user.password = hashedPassword;
+    }
+
+    const postgresUpdateUserRepository = new PostgresUpdateUserRepository();
+    const updatedUser = await postgresUpdateUserRepository.execute(
+      userId,
+      user,
+    );
+
+    return updatedUser;
+  }
+}
